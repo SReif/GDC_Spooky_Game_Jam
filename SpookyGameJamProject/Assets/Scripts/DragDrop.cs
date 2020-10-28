b5f5f79a6923968469421db3041228437a4743f9
Thy Do <thydo@asu.edu>
2020-10-28 03:06:44 -0700

    Added grid layout

---
+++ SpookyGameJamProject/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector2 origPosition;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        origPosition = rectTransform.anchoredPosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+        canvasGroup.alpha = 0.5f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDrag");
+        rectTransform.anchoredPosition = origPosition;
+        canvasGroup.alpha = 1.0f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventDate)
+    {
+        Debug.Log("PointerDown");
+    }
+}
---
+++ SpookyGameJamProject/Assets/Scripts/MouseHandling.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MouseHandling : MonoBehaviour
+{
+    public Tilemap tilemap;
+    
+    TileBase tile;
+    public TileBase greenTile;
+    
+    public Grid grid;
+
+    public Sprite sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int posInt = grid.LocalToCell(mousePos);
+        tile = tilemap.GetTile(posInt);
+        tilemap.SetColor(posInt, Color.white);
+
+        
+    }
+
+    void spawnPlant()
+    {
+        
+    }
+
+}
